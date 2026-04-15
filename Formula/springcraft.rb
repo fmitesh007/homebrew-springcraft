42e8210946ea2df377ad0a1e3cca4e574c8da90b
fmitesh007 <markhackit@gmail.com>
2026-04-16 01:50:41 +0530

    Update to v0.3.0

--- Formula/springcraft.rb
+++ Formula/springcraft.rb
@@ -1,17 +1,23 @@
 class Springcraft < Formula
   desc 'A modern CLI scaffolder for Spring Boot projects'
   homepage 'https://github.com/fmitesh007/springcraft-cli'
-  version '0.2.0'
+  version '0.3.0'
   license 'MIT'
 
   on_macos do
-    url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.2.0/springcraft-macos'
-    sha256 '1e68fbe3ca16bafd78dc79c16bb2217db649f412546791e6abe2de5c47b55048'
+    on_intel do
+      url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.0/springcraft-macos'
+      sha256 '01e8f94352e1ce62cf6255de66142b4c74b507e1406264c15e0e5955ff452d16'
+    end
+    on_arm do
+      url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.0/springcraft-macos'
+      sha256 '01e8f94352e1ce62cf6255de66142b4c74b507e1406264c15e0e5955ff452d16'
+    end
   end
 
   on_linux do
-    url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.2.0/springcraft-linux'
-    sha256 '9c0727b43e4391a2a843843d82b74e2584c0f722a6419ce3933cda12b660690f'
+    url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.0/springcraft-linux'
+    sha256 'c4443abd937afc932a9abdfd76703c6acdff09203d1033ebd65263d77118d117'
   end
 
   def install
