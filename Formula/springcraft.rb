48479856b0a680dd2babfc8cd5c66f8cb8cdb0bf
fmitesh007 <markhackit@gmail.com>
2026-04-25 01:54:57 +0530

    Update springcraft to v0.3.1
    - Update version to 0.3.1
    - Update sha256 checksums for macOS and Linux binaries

--- Formula/springcraft.rb
+++ Formula/springcraft.rb
@@ -1,23 +1,23 @@
 class Springcraft < Formula
   desc 'A modern CLI scaffolder for Spring Boot projects'
   homepage 'https://github.com/fmitesh007/springcraft-cli'
-  version '0.3.0'
+  version '0.3.1'
   license 'MIT'
 
   on_macos do
     on_intel do
-      url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.0/springcraft-macos'
-      sha256 '01e8f94352e1ce62cf6255de66142b4c74b507e1406264c15e0e5955ff452d16'
+      url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.1/springcraft-macos'
+      sha256 '7edf7c5e3506fb9cecdc7eb92acdd25c73f7450742ed85824c2994a51fa31879'
     end
     on_arm do
-      url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.0/springcraft-macos'
-      sha256 '01e8f94352e1ce62cf6255de66142b4c74b507e1406264c15e0e5955ff452d16'
+      url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.1/springcraft-macos'
+      sha256 '7edf7c5e3506fb9cecdc7eb92acdd25c73f7450742ed85824c2994a51fa31879'
     end
   end
 
   on_linux do
-    url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.0/springcraft-linux'
-    sha256 'c4443abd937afc932a9abdfd76703c6acdff09203d1033ebd65263d77118d117'
+    url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.1/springcraft-linux'
+    sha256 '6f6e27559dd995aa81519e19f4c3a96cbb3426b9a3ad7963cf54b323a3de18c7'
   end
 
   def install
